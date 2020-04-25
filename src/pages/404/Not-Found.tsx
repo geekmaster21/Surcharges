14498dd78df706feaf325a651ac74a461dd60366
fordownloads <vnukov.d03@gmail.com>
2020-04-26 02:52:45 +0800

    Add illustration to 404 page

--- src/pages/404/Not-Found.tsx
+++ src/pages/404/Not-Found.tsx
@@ -5,9 +5,12 @@ interface NotFoundProps extends RouteComponentProps { }
 
 const NotFound: React.SFC<NotFoundProps> = () => {
     return (<>
-        <Link to="/" className="link" >
-            This is the end. Nobody's here!
-        </Link>
+        <div className="hero notfound">
+            <img src="/images/404.svg" alt="404" className="nf-img"/>
+            <Link to="/" className="link" >
+                This is the end. Nobody's here!
+            </Link>
+        </div>
     </>);
 }
 
