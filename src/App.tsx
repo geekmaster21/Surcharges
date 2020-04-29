eb784d4495659d13dddae944b034de64c9632f0e
No Moderator <nomoderator@gmail.com>
2020-04-29 09:50:19 +0530

    added support for localized routing for links that don't have locale present in them

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { Redirect, Router } from "@reach/router";
+import { navigate, Redirect, Router } from "@reach/router";
 import { Splash } from "./components";
 import { STORAGE } from './core';
 import { Device, DirectBuild, Home, NotFound } from './pages';
 import './styles/style.scss';
 
 function App() {
+  const { pathname } = window.location;
   const locale = STORAGE.get<string>('lang') || 'en';
+  const langReg = /^(([a-z]{2})|([a-z]{2}-[A-Z]{2}))$/; // tests "/en" OR "en-US" lang format in url
+  const pathLang = (pathname || '').split('/').filter(Boolean).shift() || '';
+
+  if (!langReg.test(pathLang)) {
+    navigate(`/${locale}${pathname}`);
+  }
+
   return (<>
     <div></div>
     <Router style={{ height: '100%' }} >
