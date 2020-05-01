86ec0f98eb34b4cd85c1b68057b159e96a01f9ca
No Moderator <nomoderator@gmail.com>
2020-05-01 21:10:18 +0530

    google-ads regarding changes

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,23 @@ import { DarkTheme } from './themes';
 
 const theme = createMuiTheme(DarkTheme);
 
-ReactDOM.render(
-  // <React.StrictMode> TODO: Remove this once Material React fixes "findDomNode" bug
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>
-  // </React.StrictMode>
-  , document.getElementById('root')
-);
+function ConfigureGoogleAds() {
+  ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5046421731649433",
+    enable_page_level_ads: true
+  });
+}
+
+function RenderApp() {
+  ConfigureGoogleAds();
+  return (<>
+    {/* <React.StrictMode> TODO: Remove this once Material React fixes "findDomNode" bug */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+    {/* </React.StrictMode> */}
+  </>);
+}
+
+ReactDOM.render(<RenderApp />, document.getElementById('root'));
