a931874170856fd90fea74ff74b38aa5e0497e60
No Moderator <nomoderator@gmail.com>
2020-05-03 18:40:41 +0530

    removed google ads along with config

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { APP_CONFIG } from './app-config';
 import { createMuiTheme, CssBaseline, ThemeProvider } from './components';
 import { DarkTheme } from './themes';
 
 const theme = createMuiTheme(DarkTheme);
 
-function toggleGoogleAds() {
-  if (!APP_CONFIG.showAds) {
-    const adScript = document.getElementById('scr-google-ads');
-    adScript &&
-      document.head.removeChild(adScript);
-  }
-}
-
 function RenderApp() {
-  toggleGoogleAds();
   return (<>
     {/* <React.StrictMode> TODO: Remove this once Material React fixes "findDomNode" bug */}
     <ThemeProvider theme={theme}>
