a76038f509ef61709636f97551892f065e329f5f
MrYacha <yacha@orangefox.tech>
2020-08-08 21:07:40 +0300

    Languages.py: update path

--- languages.py
+++ languages.py
@@ -5,8 +5,8 @@ import glob
 
 LANGUAGES = []
 
-langs = glob.glob("public/translations/*-*.json")
-langs.append("public/translations/en.json")
+langs = glob.glob("dsite/public/translations/*-*.json")
+langs.append("dsite/public/translations/en.json")
 
 for file in langs:
     language = json.load(open(file))
@@ -23,6 +23,6 @@ for file in langs:
 
 LANGUAGES = sorted(LANGUAGES, key=itemgetter('name'))
 
-with open('public/translations/list.json', 'w+') as f:
+with open('dsite/public/translations/list.json', 'w+') as f:
     dump = json.dumps(LANGUAGES, indent=2)
     f.write(dump)
